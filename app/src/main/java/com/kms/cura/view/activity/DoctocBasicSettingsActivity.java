bb8e60566472e18acc986ee8a814ead54c0019a8
huuduy216 <duyquay123@yahoo.com.vn>
2016-06-29 16:55:49 +0700

    commit

---
+++ app/src/main/java/com/kms/cura/view/activity/DoctocBasicSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.kms.cura.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DoctocBasicSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctoc_basic_settings);
+    }
+}
